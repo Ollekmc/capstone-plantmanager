106891c7c673207d8201f8610e4456eb62e0fdf0
Kolja Willmann <93048431+Ollekmc@users.noreply.github.com>
2023-01-06 16:40:46 +0100

    Add list,findById & search function to SpeciesService.java

--- backend/src/main/java/de/backend/service/SpeciesService.java
+++ backend/src/main/java/de/backend/service/SpeciesService.java
@@ -4,6 +4,10 @@ import de.backend.model.Species;
 import de.backend.repo.SpeciesRepo;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class SpeciesService {
 
@@ -15,10 +19,29 @@ public class SpeciesService {
         this.speciesRepo = speciesRepo;
         this.idService = idService;
     }
-
+    public List<Species> list() {
+        return speciesRepo.findAll();
+    }
+    public Species findById(String id) {
+        Optional<Species> optionalSpecies = speciesRepo.findById(id);
+        if (optionalSpecies.isPresent()) {
+            return optionalSpecies.get();
+        }
+        throw new IllegalArgumentException("Id not found!");
+    }
     public Species addSpecies(Species species) {
         species.setId(idService.generateId());
         return speciesRepo.save(species);
     }
 
+    public List<Species> search(String s) {
+        List<Species> searchResultList = new ArrayList<>();
+        for (Species species: list()) {
+            if (species.getName().contains(s)){
+                searchResultList.add(species);
+            }
+        }
+
+        return searchResultList;
+    }
 }
