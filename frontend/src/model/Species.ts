4eb5cc85caef9bad9953aae3683f98d5388d9616
Kolja Willmann <93048431+Ollekmc@users.noreply.github.com>
2023-01-06 14:33:14 +0100

    Edit Species.ts w NewSpecies

--- frontend/src/model/Species.ts
+++ frontend/src/model/Species.ts
@@ -1,6 +1,24 @@
+export enum Habitat {
+    Sunny,
+    Shaded,
+    Semishaded
+}
+
+
 export type Species = {
     id: string;
     name: string;
     waterDemand: number;
     fertilizerDemand: number;
+    habitat: Habitat;
+    soil: number
+}
+
+export type NewSpecies = {
+    name: string;
+    waterDemand: number;
+    fertilizerDemand: number;
+    habitat: Habitat;
+    soil: number;
+
 }
