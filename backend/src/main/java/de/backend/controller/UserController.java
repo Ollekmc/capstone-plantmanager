fab6d14f00119b5eb57913ebd98808e269bf923a
Kolja Willmann <93048431+Ollekmc@users.noreply.github.com>
2023-01-08 21:27:56 +0100

    Add UserController.java

---
+++ backend/src/main/java/de/backend/controller/UserController.java
@@ -0,0 +1,26 @@
+package de.backend.controller;
+
+import de.backend.model.User;
+import de.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping()
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+}
