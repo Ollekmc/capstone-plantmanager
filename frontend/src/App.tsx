ef8f78cfd46e93c7f02664c10e56b4fe40d466a4
Kolja Willmann <93048431+Ollekmc@users.noreply.github.com>
2023-01-09 08:36:31 +0100

    Edit App.tsx to add UserApp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import SpeciesApp from "./component/SpeciesApp";
+import UserApp from "./component/UserApp";
 
 function App() {
   return (
     <div>
         <h1>Manage you plants<p/></h1>
+        <UserApp/>
         <SpeciesApp/>
     </div>
   );
