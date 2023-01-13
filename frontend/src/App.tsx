4cc349ecb33537a6b2f49f38cd7fd054b8ab22a3
Kolja Willmann <93048431+Ollekmc@users.noreply.github.com>
2023-01-13 16:46:25 +0100

    Add temporary routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,27 @@ import React from 'react';
 import './App.css';
 import SpeciesApp from "./component/SpeciesApp";
 import UserApp from "./component/UserApp";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NavBar from "./component/NavBar";
+import UserDetails from "./component/UserDetails";
+
 
 function App() {
+
+
+
   return (
     <div>
+
+        <BrowserRouter>
+            <NavBar/>
+            <Routes>
+                <Route path={"/plants"} element={SpeciesApp()}/>
+                <Route path={"/users"} element={UserApp()}/>
+                <Route path={"/users/:id"} element={<UserDetails/>}></Route>
+            </Routes>
+        </BrowserRouter>
         <h1>Manage you plants<p/></h1>
-        <UserApp/>
-        <SpeciesApp/>
     </div>
   );
 }
