ee2ca4faf70daf4127fe1e557241bf799dea30ba
Kolja Willmann <93048431+Ollekmc@users.noreply.github.com>
2023-01-13 16:39:38 +0100

    Edit UserCard.tsx to show Details and Delete

--- frontend/src/component/UserCard.tsx
+++ frontend/src/component/UserCard.tsx
@@ -1,13 +1,31 @@
 import {User} from "../model/User";
+import './UserCard.css'
+import {useNavigate} from "react-router-dom";
 
 type UserCardProps = {
     user: User
+    removeUser: (id:string) => void
 }
 
 export default function UserCard(props: UserCardProps) {
+
+    const navigate = useNavigate();
+
+    function handleDetailsClick() {
+        navigate("/users/"+props.user.id)
+    }
+    function handleDelete(){
+        props.removeUser(props.user.id)
+    }
+
     return (
-        <div>
-            <p className={"user-card__name"}>{props.user.name}</p>
+        <div className={"user-card"}>
+            <p className={"user-card__name"}>{props.user.name}</p><br/>
+            <div className={"user-card__buttons"}><br/>
+                <button className={"unused-button"}>Edit</button>
+                <button className={"used-button"} onClick={handleDelete}>Delete</button>
+                <button className={"used-button"} onClick={handleDetailsClick}>Details</button>
+            </div>
         </div>
     )
 }
