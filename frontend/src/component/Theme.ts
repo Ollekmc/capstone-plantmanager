0d53ed379fdc3970f0605593308bfee1a53a90b2
Kolja Willmann <93048431+Ollekmc@users.noreply.github.com>
2023-01-17 20:18:37 +0100

    Add Theme.ts First draft of MUI-Theme

---
+++ frontend/src/component/Theme.ts
@@ -0,0 +1,16 @@
+import {ThemeOptions} from "@mui/material";
+
+export const themeOptions: ThemeOptions = {
+    palette: {
+        primary: {
+            main: '#c7e5c8',
+            light: '#fafffb',
+            dark: '#96b397',
+        },
+        secondary: {
+            main: '#ffcdd2',
+            light: '#ffffff',
+            dark: '#cb9ca1',
+        },
+    },
+};
